cc1bb6fc80030c8e4d49494a5cd9c1886a1d75ae
evgeniy volkov <volkov.e.n@gmail.com>
2021-03-11 15:50:10 +0300

    init commit

---
+++ src/test/java/ru/volkov/batch/listeners/ListenersApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.volkov.batch.listeners;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListenersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
